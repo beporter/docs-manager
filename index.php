d20731ec510497f550776d55b9d70cf3840704c0
Brian Porter <beporter@users.sourceforge.net>
2014-10-09 10:50:55 -0500

    More tweaks.

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
 
 // main()
 
+// Error if $authToken not set.
+// Error if $basePath not writeable.
+
+
 // If POST and AUTH_TOKEN is valid and PROJECT_NAME is present.
 //  Slugify PROJECT_NAME if necessary to make it filesystem path safe.
-//  Create a folder for $basePath + $projectSlug if it doesn't already exist.
-//  Save POSTed ZIP to tmp.
-//  Extract ZIP to destination path.
-//  Write an .htaccess file for Basic Auth using $projectSlug for the username and (something?) for password. (These credentials must be shareable with clients and must be unique to prevent one client from accessing another client's docs.)
+//  Create a folder for $basePath + $projectSlug if it doesn't already exist. (Error on failure.)
+//  Save POSTed ZIP to tmp. (Error on failure.)
+//  Extract ZIP to destination path. (Error on failure.)
+//  Write an .htaccess file for Basic Auth using $projectSlug for the username and (something?) for password.
+//     (These credentials must be shareable with clients and must be unique to prevent one client from
+//     accessing another client's docs.) (Error on failure.)
 // Else
-//  No output?
+//  No output? Help output (in json)?
 
 // Return json to caller with status.
